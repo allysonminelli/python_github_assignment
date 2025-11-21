28b6c96d664f6fd3b63c6b1fbb8b2d8481d155fe
allysonminelli <allysonleighminelli@usf.edu>
2025-11-20 20:04:16 -0500

    convert to floar

--- app.py
+++ app.py
@@ -2,3 +2,9 @@
 print("Welcome to my Python program")
 #Ask for daily sales
 sales = input("How many sales did you make today")
+#convert to flaot
+try:
+  hours = float(hours)
+except ValuieError:
+  print(please enter a valid number.")
+  exit()
